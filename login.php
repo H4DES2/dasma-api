7a9ae33dc088027dbf6bf84f6e7a5c554d4bc478
H4DES2 <jacobbataclanortega@gmail.com>
2026-09-05 17:40:44 +0800

    Remove duplicate PHP CORS header

--- login.php
+++ login.php
@@ -1,8 +1,4 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
-
 // Handle preflight OPTIONS request sent by the browser
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
