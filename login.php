feca393382ade9dc69dc533ce4166baf3aa24003
H4DES2 <jacobbataclanortega@gmail.com>
2026-09-05 17:19:29 +0800

    Add CORS headers and preflight handling

--- login.php
+++ login.php
@@ -1,4 +1,14 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+
+// Handle preflight OPTIONS request sent by the browser
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 require_once __DIR__ . '/config.php';
 header("Content-Type: application/json; charset=UTF-8");
 
